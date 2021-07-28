0b0e7b08218c502bfb541308a10c9958782e75d2
Haikun Xu <haikunxu1990@gmail.com>
2021-07-28 12:56:02 -0700

    update the example code

--- demo/Example_Code.R
+++ demo/Example_Code.R
@@ -19,32 +19,37 @@ head(LF_Tree)
 # The last few columns with names Flag are the cell numbers under each split
 
 
-# plot results
-library(tidyverse)
 
-# plot the spatial distribution of each cell
-for (i in 1:Nsplit) {
-  LF_Tree$Flag <- LF_Tree[[paste0("Flag",i)]]
-  f <- ggplot(data=LF_Tree) +
-    geom_point(aes(x=lon,y=lat,color=as.factor(Flag))) +
-    theme_bw()
 
-  ggsave(f,file=paste0(i,"Flag.png"),width=6,height=6)
-}
 
-# plot the mean LF of each cell
-for (i in 1:Nsplit) {
-  LF_Tree$Flag <- LF_Tree[[paste0("Flag",i)]]
 
-  LF_Tree_final <- LF_Tree %>% gather(fcol:lcol,key="Length",value = "LF_Tree2") %>%
-    mutate(Length=as.numeric(Length)) %>%
-    group_by(Flag,Length) %>%
-    summarise(LF_Tree3=mean(LF_Tree2))
 
-  f <- ggplot(data=LF_Tree_final) +
-    geom_line(aes(x=Length,y=LF_Tree3,color=as.factor(Flag))) +
-    theme_bw()
-    # facet_wrap(~Flag)
-
-  ggsave(f,file=paste0(i,"LF.png"),width=6,height=6)
-}
+# backup code
+# library(tidyverse)
+#
+# # plot the spatial distribution of each cell
+# for (i in 1:Nsplit) {
+#   LF_Tree$Flag <- LF_Tree[[paste0("Flag",i)]]
+#   f <- ggplot(data=LF_Tree) +
+#     geom_point(aes(x=lon,y=lat,color=as.factor(Flag))) +
+#     theme_bw()
+#
+#   ggsave(f,file=paste0(i,"Flag.png"),width=6,height=6)
+# }
+#
+# # plot the mean LF of each cell
+# for (i in 1:Nsplit) {
+#   LF_Tree$Flag <- LF_Tree[[paste0("Flag",i)]]
+#
+#   LF_Tree_final <- LF_Tree %>% gather(fcol:lcol,key="Length",value = "LF_Tree2") %>%
+#     mutate(Length=as.numeric(Length)) %>%
+#     group_by(Flag,Length) %>%
+#     summarise(LF_Tree3=mean(LF_Tree2))
+#
+#   f <- ggplot(data=LF_Tree_final) +
+#     geom_line(aes(x=Length,y=LF_Tree3,color=as.factor(Flag))) +
+#     theme_bw()
+#     # facet_wrap(~Flag)
+#
+#   ggsave(f,file=paste0(i,"LF.png"),width=6,height=6)
+# }
