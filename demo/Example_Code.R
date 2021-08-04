8483d3624867bf6ce07f9600646ff221c8f0a192
Haikun Xu <haikunxu1990@gmail.com>
2021-08-04 16:37:51 -0700

    update the example code based on recent major updates

--- demo/Example_Code.R
+++ demo/Example_Code.R
@@ -13,52 +13,29 @@ lcol <- 17 # the last column with LF_Tree info
 Nsplit <- 3 # the number of splits (the number of cells - 1)
 save_dir <- "demo/"
 
-# run the regression tree
+# run the regression tree with the best three splits
+# results are saved in the folder 111 under save_dir
 LF_Tree <- run_regression_tree(LF,fcol,lcol,Nsplit,save_dir)
 
 head(LF_Tree$LF)
 # The last few columns with names Flag are the cell numbers under each split
 
-# run the regression tree
-LF_Tree <- run_regression_tree(LF,fcol,lcol,Nsplit,save_dir,lat.min=1,lon.min=2)
+# run the regression tree again with the second best 2th split
+# results are saved in the folder 121 under save_dir
+LF_Tree <- run_regression_tree(LF,fcol,lcol,Nsplit,save_dir,manual = TRUE, select = c(1,2,1))
 
-# loop the regression tree
+# This setting leads to even better improvement (16.4% vs. 16.2% variance explained)
+# than the default run
+
+# loop the regression tree for various combinations of splits
 Var <- loop_regression_tree(LF,fcol,lcol,Nsplit,save_dir,max_select = 3)
 
+# column Var is the % variance explained by each split combination
 Var
+# the best splits is 1, 2, 1
 
-# user-specified regression tree
-select <- as.numeric(Var[1,1:Nsplit]) # use the best 1st, 2nd, and 4th splits and the second best 3rd split
+# use the best combination of splits found above through the loop
+select <- as.numeric(Var[1,1:Nsplit]) # the first row has the highest % variance explained
 
 LF_Tree <- run_regression_tree(LF,fcol,lcol,Nsplit,save_dir,manual = TRUE, select)
-
-
-# backup code
-# library(tidyverse)
-#
-# # plot the spatial distribution of each cell
-# for (i in 1:Nsplit) {
-#   LF_Tree$Flag <- LF_Tree[[paste0("Flag",i)]]
-#   f <- ggplot(data=LF_Tree) +
-#     geom_point(aes(x=lon,y=lat,color=as.factor(Flag))) +
-#     theme_bw()
-#
-#   ggsave(f,file=paste0(i,"Flag.png"),width=6,height=6)
-# }
-#
-# # plot the mean LF of each cell
-# for (i in 1:Nsplit) {
-#   LF_Tree$Flag <- LF_Tree[[paste0("Flag",i)]]
-#
-#   LF_Tree_final <- LF_Tree %>% gather(fcol:lcol,key="Length",value = "LF_Tree2") %>%
-#     mutate(Length=as.numeric(Length)) %>%
-#     group_by(Flag,Length) %>%
-#     summarise(LF_Tree3=mean(LF_Tree2))
-#
-#   f <- ggplot(data=LF_Tree_final) +
-#     geom_line(aes(x=Length,y=LF_Tree3,color=as.factor(Flag))) +
-#     theme_bw()
-#     # facet_wrap(~Flag)
-#
-#   ggsave(f,file=paste0(i,"LF.png"),width=6,height=6)
-# }
+head(LF_Tree$LF)
