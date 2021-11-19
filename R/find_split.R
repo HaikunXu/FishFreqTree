041b6e2826dd3eab2316ed982012e0e4ef552279
Haikun Xu <haikunxu1990@gmail.com>
2021-11-19 11:18:22 -0800

    fix a typo

--- R/find_split.R
+++ R/find_split.R
@@ -14,23 +14,23 @@ find_split <- function(lf_prop,frstcol.lf,lstcol.lf,lat.min,lon.min,year.min,qua
   tmpcl.kld <- simult.tree.kld.FINAL(lf_prop,frstcol.lf,lstcol.lf,lat.min=lat.min,lon.min=lon.min,year.min = year.min)
 
   # combine the result tables into a single data frame
-  tmpcl.kld.lat <- data.frame("Improve"=tmpcl.kld$lf.lat[,1],
+  tmpcl.kld.lat <- data.frame("Improvement"=tmpcl.kld$lf.lat[,1],
                               "Key"="Lat",
                               "Value"=tmpcl.kld$lf.lat[,2])
-  tmpcl.kld.lon <- data.frame("Improve"=tmpcl.kld$lf.lon[,1],
+  tmpcl.kld.lon <- data.frame("Improvement"=tmpcl.kld$lf.lon[,1],
                               "Key"="Lon",
                               "Value"=tmpcl.kld$lf.lon[,2])
 
   if(quarter==TRUE) {
 
   if((is.null(tmpcl.kld$lf.cyclic.qrtr)==TRUE)&(is.null(tmpcl.kld$lf.qrtr)==FALSE)) {
-    tmpcl.kld.qrt <- data.frame("Improve"=tmpcl.kld$lf.qrtr[,1],
+    tmpcl.kld.qrt <- data.frame("Improvement"=tmpcl.kld$lf.qrtr[,1],
                                 "Key"="Qrt",
                                 "Value"=tmpcl.kld$lf.qrtr[,2])
     tmpcl.kld.df <- rbind(tmpcl.kld.lat,tmpcl.kld.lon,tmpcl.kld.qrt)
   }
   if((is.null(tmpcl.kld$lf.cyclic.qrtr)==FALSE)&(is.null(tmpcl.kld$lf.qrtr)==TRUE)) {
-    tmpcl.kld.cqrt <- data.frame("Improve"=tmpcl.kld$lf.cyclic.qrtr[,1],
+    tmpcl.kld.cqrt <- data.frame("Improvement"=tmpcl.kld$lf.cyclic.qrtr[,1],
                                  "Key"="CQrt",
                                  "Value"=as.numeric(tmpcl.kld$lf.cyclic.qrtr[,2]))
     tmpcl.kld.df <- rbind(tmpcl.kld.lat,tmpcl.kld.lon,tmpcl.cyclic.kld.qrt)
@@ -39,10 +39,10 @@ find_split <- function(lf_prop,frstcol.lf,lstcol.lf,lat.min,lon.min,year.min,qua
     tmpcl.kld.df <- rbind(tmpcl.kld.lat,tmpcl.kld.lon)
   }
   if((is.null(tmpcl.kld$lf.cyclic.qrtr)==FALSE)&(is.null(tmpcl.kld$lf.qrtr)==FALSE)) {
-    tmpcl.kld.cqrt <- data.frame("Improve"=tmpcl.kld$lf.cyclic.qrtr[,1],
+    tmpcl.kld.cqrt <- data.frame("Improvement"=tmpcl.kld$lf.cyclic.qrtr[,1],
                                  "Key"="CQrt",
                                  "Value"=as.numeric(tmpcl.kld$lf.cyclic.qrtr[,2]))
-    tmpcl.kld.qrt <- data.frame("Improve"=tmpcl.kld$lf.qrtr[,1],
+    tmpcl.kld.qrt <- data.frame("Improvement"=tmpcl.kld$lf.qrtr[,1],
                                 "Key"="Qrt",
                                 "Value"=tmpcl.kld$lf.qrtr[,2])
     tmpcl.kld.df <- rbind(tmpcl.kld.lat,tmpcl.kld.lon,tmpcl.kld.qrt,tmpcl.kld.cqrt)
@@ -51,7 +51,7 @@ find_split <- function(lf_prop,frstcol.lf,lstcol.lf,lat.min,lon.min,year.min,qua
   else {tmpcl.kld.df <- rbind(tmpcl.kld.lat,tmpcl.kld.lon)}
 
   if (year==TRUE) {
-    tmpcl.kld.year <- data.frame("Improve"=tmpcl.kld$lf.year[,1],
+    tmpcl.kld.year <- data.frame("Improvement"=tmpcl.kld$lf.year[,1],
                                 "Key"="Year",
                                 "Value"=tmpcl.kld$lf.year[,2])
     tmpcl.kld.df <- rbind(tmpcl.kld.df,tmpcl.kld.year)
