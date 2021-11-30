8cdaa173f1be3473b74044567ab065535143286a
Haikun Xu <haikunxu1990@gmail.com>
2021-11-29 16:25:33 -0800

    update the function description

--- R/run.regression.tree.R
+++ R/run.regression.tree.R
@@ -1,6 +1,6 @@
 #' Run the regression tree analysis with specified number of splits
 #'
-#' \code{run_regression_tree} This function generates an area code for longline catch allocation
+#' \code{run_regression_tree} This function is the main regression tree function
 #'
 #' @param LF The length frequency data frame input; must include four columns: lat, lon, year, and quarter
 #' @param fcol The first column in the data frame with length frequency info
